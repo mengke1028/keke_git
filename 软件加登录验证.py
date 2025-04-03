92219b091d10c3bdf855b220552fe5582cb66011
keke <mengke1028@163.com>
2025-04-03 15:21:13 +0800

    整合mysql

--- 软件加登录验证.py
+++ 软件加登录验证.py
@@ -2,45 +2,114 @@ import os
 from tkinter import ttk
 import tkinter as tk
 from tkinter import messagebox
-from 连接Mysql数据库 import MySQLDatabase
 import webbrowser
+import mysql.connector
+from mysql.connector import Error
+import time
 
-
-# 预设的账号和密码
-class UI:
+class MySQLDatabase:
     def __init__(self):
-        # 创建主窗口
-        self.root = None
-        self.entry = None
-        self.frame = None
+        self.host = '106.54.233.105'
+        self.database = 'keke_test'
+        self.user = 'keke'
+        self.password = 'mengke1028..'
+        self.connection = None
+        self.create_connection()
 
-    # 主程序窗口
-    def open_main_app(self):
-        self.root = tk.Tk()
-        self.root.title("TTK 示例程序")
-        self.frame = ttk.Frame(self.root, padding="10")
-        self.entry = ttk.Entry(self.frame)
-        # 创建一个框架，用于组织控件
-        self.frame.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
-        # 创建一个标签
-        label = ttk.Label(self.frame, text="请输入你的名字:")
-        label.grid(row=0, column=0, padx=5, pady=5)
-        # 创建一个输入框
-        self.entry.grid(row=0, column=1, padx=5, pady=5)
+    def create_connection(self):
+        try:
+            self.connection = mysql.connector.connect(
+                host=self.host,
+                database=self.database,
+                user=self.user,
+                password=self.password
+            )
+            if self.connection.is_connected():
+                print('Connected to MySQL database')
+        except Error as e:
+            print(f"Error while connecting to MySQL: {e}")
 
-        # 定义一个函数，用于处理按钮点击事件
-        def show_name():
-            name = self.entry.get()
-            result_label.config(text=f"你输入的名字是: {name}")
+    def add_code(self, key):
+        # 往数据库增加卡密
+        insert_query = f"INSERT INTO activation_codes (activation_code, start_time, is_destroyed) VALUES ('{key}', NULL ,0)"
+        cursor = self.connection.cursor()
+        if insert_query:
+            cursor.execute(insert_query)
+            self.connection.commit()
+            print("Query executed successfully")
 
-        # 创建一个按钮
-        button = ttk.Button(self.frame, text="显示名字", command=show_name)
-        button.grid(row=1, column=0, columnspan=2, pady=10)
-        # 创建一个用于显示结果的标签
-        result_label = ttk.Label(self.frame, text="")
-        result_label.grid(row=2, column=0, columnspan=2, pady=5)
-        # 运行主循环
-        self.root.mainloop()
+    def read_query(self):
+        # 查询全部的数据
+        query = "SELECT * FROM activation_codes"
+        if self.connection:
+            cursor = self.connection.cursor()
+            result = None
+            try:
+                cursor.execute(query)
+                result = cursor.fetchall()
+                return result
+            except Error as e:
+                print(f"Error while reading query: {e}")
+
+    def close_connection(self):
+        if self.connection and self.connection.is_connected():
+            self.connection.close()
+            print('Connection to MySQL database closed')
+
+    def update_code(self, key):
+        """更新卡密状态为 1 并记录当前时间 """
+        current_time = str(int(time.time()))
+        update_query = "UPDATE activation_codes SET start_time = %s, is_destroyed = 1 WHERE activation_code = %s"
+        values = (current_time, key)
+
+        cursor = self.connection.cursor()
+        cursor.execute(update_query, values)
+        rows_affected = cursor.rowcount
+        self.connection.commit()
+        if rows_affected == 0:
+            print(f"卡密 {key} 可能不存在")
+            return False
+        else:
+            return True
+
+    def query_by_activation_code(self, activation_code):
+        """根据 activation_code 查询记录"""
+        query = "SELECT * FROM activation_codes WHERE activation_code = %s"
+        values = (activation_code,)
+        if self.connection:
+            cursor = self.connection.cursor()
+            try:
+                cursor.execute(query, values)
+                result = cursor.fetchall()
+                return result
+            except Error as e:
+                print(f"查询数据时出现错误: {e}")
+        return None
+
+    def shiyongkami(self, activation_code):
+        """使用卡密 """
+        data = self.query_by_activation_code(activation_code)
+
+        if not data:
+            return '卡密不存在'
+        stat_time = data[0][1]
+        if stat_time is None:
+            # 执行修改
+            try:
+                self.update_code(activation_code)
+                print(f'{activation_code} 状态修改成 1, 记录时间')
+                return True, f'卡密还有{4}小时过期'
+            except Exception as e:
+                print(e)
+                print(f"卡密 {activation_code} 可能不存在")
+                return '卡密错误'
+        else:
+            low_time = int(time.time()) - int(stat_time)
+            if low_time > 14400:  # 4小时试用期
+                return '试用超时了'
+            else:
+                print(f'卡密还有{int((14400-low_time)/3600)}小时过期')
+                return True, f'卡密还有{int((14400-low_time)/3600)}小时过期'
 
 
 def open_baidu(event):
@@ -128,5 +197,3 @@ if __name__ == '__main__':
     if not login.run():
         exit()
 
-    ui = UI()
-    ui.open_main_app()
